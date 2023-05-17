6405e69c6434139011f43f3b1dac7ca86c26b9f7
Mike Katsufrakis <mike.k@netvendor.com>
2023-05-16 17:35:33 -0700

    more menu styles for publications

--- src/components/Publication/index.jsx
+++ src/components/Publication/index.jsx
@@ -16,13 +16,23 @@ const Publication = () => {
   return (
     <div className="pubgrid">
       <div>
-        <h2>{publication?.name}</h2>
-        <ul>
+        <h2 className="publication-title">{publication?.name}</h2>
+        <ul className="publication-menu">
           {articles?.map((a, i) => {
             return (
-              <li key={i}>
+              <li className="publication-menu__item" key={i}>
                 <NavLink to={`${a.id}`}>
-                  {a.order} {a.title}
+                  <div className="publication-order">
+                    <span>{a.order}</span>
+                  </div>{" "}
+                  <div>
+                    {a.authorShort && (
+                      <div className="publication-article-author">
+                        {a.authorShort}
+                      </div>
+                    )}
+                    <div className="publication-article-title">{a.title}</div>
+                  </div>
                 </NavLink>
               </li>
             );
